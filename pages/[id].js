17f82ef2417e828f1fe977717bf0530e2561af9c
Archie Baer <archie@abaer.dev>
2020-03-07 18:23:32 +0000

    Formatting

--- pages/[id].js
+++ pages/[id].js
@@ -8,99 +8,125 @@ import marked from "marked";
 import insane from "insane";
 import moment from "moment";
 import axios from "axios";
-import {withRouter} from "next/router";
+import { withRouter } from "next/router";
 import Link from "next/link";
 
 marked.setOptions({
   breaks: true,
-  highlight: (code, lang) => (lang && hljs.getLanguage(lang)) ? hljs.highlight(lang, code).value : hljs.highlightAuto(code).value
+  highlight: (code, lang) =>
+    lang && hljs.getLanguage(lang)
+      ? hljs.highlight(lang, code).value
+      : hljs.highlightAuto(code).value
 });
 
-const DocPage = props => props.doc ? (
-  <Page
-    title={props.doc.id}
-    user={props.user}
-    breadcrumbs={[
-      {
-        name: props.doc.id
-      }
-    ]}
-  >
-    <header>
-      <h1>{props.doc.name}</h1>
-      <p title={props.doc.editedAt ? `Edited at ${moment(props.doc.editedAt).format("LLL")}` : "Never Edited"}>By <a href={`https://alles.cx/u/${props.doc.author.username}`}>@{props.doc.author.username}</a> at {moment(props.doc.createdAt).format("LLL")}</p>
-    </header>
-    <div
-      className="content"
-      dangerouslySetInnerHTML={{ __html: props.doc.html }}
-    ></div>
+const DocPage = props =>
+  props.doc ? (
+    <Page
+      title={props.doc.id}
+      user={props.user}
+      breadcrumbs={[
+        {
+          name: props.doc.id
+        }
+      ]}
+    >
+      <header>
+        <h1>{props.doc.name}</h1>
+        <p
+          title={
+            props.doc.editedAt
+              ? `Edited at ${moment(props.doc.editedAt).format("LLL")}`
+              : "Never Edited"
+          }
+        >
+          By{" "}
+          <a href={`https://alles.cx/u/${props.doc.author.username}`}>
+            @{props.doc.author.username}
+          </a>{" "}
+          at {moment(props.doc.createdAt).format("LLL")}
+        </p>
+      </header>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: props.doc.html }}
+      ></div>
 
-    <style jsx>{`
-      header {
-        background: ${theme.foreground};
-        padding: 10px;
-        box-sizing: border-box;
-      }
+      <style jsx>{`
+        header {
+          background: ${theme.foreground};
+          padding: 10px;
+          box-sizing: border-box;
+        }
 
-      header h1 {
-        margin: 0;
-        font-size: 20px;
-      }
+        header h1 {
+          margin: 0;
+          font-size: 20px;
+        }
 
-      header p {
+        header p {
           margin: 0;
           margin-top: 5px;
           font-size: 10px;
-      }
+        }
 
-      header a {
+        header a {
           font-weight: bold;
-      }
+        }
 
-      .content {
-        padding: 10px;
-        white-space: ${props.doc.markdown ? "normal" : "pre-wrap"};
-      }
-    `}</style>
-  </Page>
-) : (
-  <Page
-    title="ERROR"
-    user={props.user}
-  >
-    <header>
-      <h1>404: Document Not Found</h1>
-    </header>
-    <div className="content">
-      <p>This document could not be found. Perhaps it's expired or been deleted.</p>
-      <p>Perhaps you could <Link href="/new"><a>create a new one</a></Link>.</p>
-    </div>
+        .content {
+          padding: 10px;
+          white-space: ${props.doc.markdown ? "normal" : "pre-wrap"};
+        }
+      `}</style>
+    </Page>
+  ) : (
+    <Page title="ERROR" user={props.user}>
+      <header>
+        <h1>404: Document Not Found</h1>
+      </header>
+      <div className="content">
+        <p>
+          This document could not be found. Perhaps it's expired or been
+          deleted.
+        </p>
+        <p>
+          Perhaps you could{" "}
+          <Link href="/new">
+            <a>create a new one</a>
+          </Link>
+          .
+        </p>
+      </div>
 
-    <style jsx>{`
-      header {
-        background: ${theme.foreground};
-        padding: 10px;
-        box-sizing: border-box;
-      }
+      <style jsx>{`
+        header {
+          background: ${theme.foreground};
+          padding: 10px;
+          box-sizing: border-box;
+        }
 
-      header h1 {
-        margin: 0;
-        font-size: 20px;
-      }
+        header h1 {
+          margin: 0;
+          font-size: 20px;
+        }
 
-      .content {
-        padding: 10px;
-      }
-    `}</style>
-  </Page>
-);
+        .content {
+          padding: 10px;
+        }
+      `}</style>
+    </Page>
+  );
 
 DocPage.getInitialProps = async ctx => {
   var doc;
   try {
-    doc = (await axios.get(`${config.apiUrl}/doc/${encodeURIComponent(ctx.query.id)}`)).data;
+    doc = (
+      await axios.get(
+        `${config.apiUrl}/doc/${encodeURIComponent(ctx.query.id)}`
+      )
+    ).data;
   } catch (err) {
-    return {doc: null};
+    return { doc: null };
   }
 
   if (doc.highlight && doc.markdown) {
